3755293f3ccb9323df8fb3f47dfa4cfd09edd6ae
Yazmin <jazmiin.1904@gmail.com>
2019-12-13 20:18:17 -0600

    Crud

---
+++ src/Entity/Persona.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PersonaRepository")
+ */
+class Persona
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nombre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $apellidoPaterno;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $apellidoMaterno;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $fechaNacimiento;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getApellidoPaterno(): ?string
+    {
+        return $this->apellidoPaterno;
+    }
+
+    public function setApellidoPaterno(string $apellidoPaterno): self
+    {
+        $this->apellidoPaterno = $apellidoPaterno;
+
+        return $this;
+    }
+
+    public function getApellidoMaterno(): ?string
+    {
+        return $this->apellidoMaterno;
+    }
+
+    public function setApellidoMaterno(string $apellidoMaterno): self
+    {
+        $this->apellidoMaterno = $apellidoMaterno;
+
+        return $this;
+    }
+
+    public function getFechaNacimiento(): ?\DateTimeInterface
+    {
+        return $this->fechaNacimiento;
+    }
+
+    public function setFechaNacimiento(\DateTimeInterface $fechaNacimiento): self
+    {
+        $this->fechaNacimiento = $fechaNacimiento;
+
+        return $this;
+    }
+}
